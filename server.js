d57d00e0376f8f48d77f3ad4b0e82e6fcdddd9b8
Amrutha Reddy Karumuru <akarumur@uncc.edu>
2024-02-25 23:57:07 -0500

    JWT changes

--- server.js
+++ server.js
@@ -64,28 +64,6 @@ app.post('/api/login',(req,res) => {
     }
 })
 
-// app.post('/api/login', (req, res) => {
-//     const { username, password } = req.body;
-    
-//     for (let user of users) {
-//         if (username == user.username && password == user.password) {
-//            let token = jwt.sign({ id: user.id, username: user.username }, SecretKey, { expiresIn: '3m' });
-//         res.json({
-//             success: true,
-//             err: null,
-//             token
-//         });
-//     break;
-//     } 
-//     else {
-//         res.status(401).json({
-//             success: false,
-//             token: null,
-//             err: 'Username or password is incorrect'
-//         });
-//     }
-// }
-// });
 app.get('/api/dashboard', jwtMW, (req, res) => {
     res.json({
         success: true,
